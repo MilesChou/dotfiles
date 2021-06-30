23d1d55190aff749e07ed8bec62375ed0c36917f
MilesChou <jangconan@gmail.com>
2021-06-30 09:58:37 +0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
-# Prepend paths
 set prepend_paths
 set prepend_paths $prepend_paths /usr/local/opt/ruby/bin
 set prepend_paths $prepend_paths /usr/local/opt/go/libexec/bin
 set prepend_paths $prepend_paths /usr/local/sbin
 set prepend_paths $prepend_paths $HOME/.composer/vendor/bin
+set prepend_paths $prepend_paths $HOME/.gem/ruby/2.6.0/bin
 set prepend_paths $prepend_paths $HOME/go/bin
 set prepend_paths $prepend_paths $HOME/bin
 
@@ -15,11 +15,23 @@ for val in $prepend_paths
     end
 end
 
-# nvm, need fish-nvm https://github.com/FabioAntunes/fish-nvm
+# JAVA Home
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
+# lang
+set -x LANG en_US.UTF-8
+
+# nvm
 if test -d (brew --prefix nvm)
     set -x NVM_DIR $HOME/.nvm
     set -x nvm_prefix (brew --prefix nvm)
 end
+
+# init Node
+node -v > /dev/null
+
+# init phpbrew
+source ~/.phpbrew/phpbrew.fish
 
 # thefuck
 if test -d (brew --prefix thefuck)
@@ -27,9 +39,9 @@ if test -d (brew --prefix thefuck)
 end
 
 # Powerline
-# set fish_function_path $fish_function_path "/usr/local/lib/python3.7/site-packages/powerline/bindings/fish"
+# set fish_function_path $fish_function_path "/usr/local/lib/python3.9/site-packages/powerline/bindings/fish"
 # powerline-setup
 
-# Pure Fish
+# THEME PURE #
 set fish_function_path /Users/miles/.config/fish/functions/theme-pure/functions/ $fish_function_path
 source /Users/miles/.config/fish/functions/theme-pure/conf.d/pure.fish
